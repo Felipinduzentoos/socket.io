22c7f1257f0141a405dc0ffcce7af9820c3d0d70
Felipinduzentoos <felipin200dedano@gmail.com>
2024-09-06 10:55:40 -0300

    commit 4

--- main/server.js
+++ main/server.js
@@ -4,7 +4,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/main'));
+app.use(express.static(__dirname + '/'));
 
 function onConnection(socket){
   socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
